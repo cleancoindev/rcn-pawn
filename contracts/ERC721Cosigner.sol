2653a75c1f951d9f82bcade41f75c89f6032de30
rotcivegaf <victorfage@gmail.com>
2018-07-11 12:02:31 -0300

    fix ERC721Cosigner and test

--- contracts/ERC721Cosigner.sol
+++ contracts/ERC721Cosigner.sol
@@ -3,30 +3,30 @@ pragma solidity ^0.4.15;
 import "./rcn/interfaces/Cosigner.sol";
 import "./rcn/interfaces/Engine.sol";
 import "./rcn/interfaces/Token.sol";
-import "./rcn/interfaces/ERC721.sol";
 import "./rcn/utils/BytesUtils.sol";
 import "./rcn/utils/Ownable.sol";
+import "./interfaces/ERC721.sol";
 
 contract ERC721Cosigner is Cosigner, BytesUtils, Ownable {
-    mapping(uint => ERC721Data) loanIdToERC721Data;
+    mapping(uint => ERC721Data) loanIdToERC721Data; // loan index on loans array in nano loan engine to ERC721Data
     struct ERC721Data {
         ERC721 addr; // address of ERC721 contract
         uint nft; // non fungible id
     }
-
+    // index of metadata
     uint private constant INDEX_ERC721_ADDR = 0;
     uint private constant INDEX_NFT = 1;
-
+    //this cosign dont have cost
     function cost(address , uint256 , bytes , bytes ) public view returns (uint256) {
         return 0;
     }
 
     function requestCosign(Engine _engine, uint256 _index, bytes _data, bytes ) public returns (bool) {
-        require(msg.sender == address(_engine));
+        require(msg.sender == address(_engine), "the sender its not the Engine");
 
         ERC721 erc721 = ERC721(address(readBytes32(_data, INDEX_ERC721_ADDR)));
         uint nft = uint(readBytes32(_data, INDEX_NFT));
-        require(erc721.takeOwnership(nft));
+        require(erc721.transferFrom(_engine.getBorrower(_index), address(this), nft), "fail transfer");
         loanIdToERC721Data[_index] = ERC721Data(erc721, nft);
 
         require(_engine.cosign(_index, 0), "fail cosing");
@@ -52,21 +52,18 @@ contract ERC721Cosigner is Cosigner, BytesUtils, Ownable {
     function claim(address _engineAddress, uint256 _index, bytes ) public returns (bool) {
       Engine engine = Engine(_engineAddress);
       ERC721Data storage erc721Data = loanIdToERC721Data[_index];
-
-      address lender = engine.ownerOf(_index);
-      if(msg.sender == lender) {
-          require(isDefaulted(engine, _index));
-          erc721Data.addr.transfer(lender, erc721Data.nft);
+      // for borrower claim
+      address borrower = engine.getBorrower(_index);
+      if(msg.sender == borrower) {
+          require(engine.getStatus(_index) == Engine.Status.paid, "the loan its not paid");
+          require(erc721Data.addr.transferFrom(address(this), borrower, erc721Data.nft), "fail transfer");
           return true;
-      } else {
-          address borrower = engine.getBorrower(_index);
-          if(msg.sender == borrower){
-              require(engine.getStatus(_index) == Engine.Status.paid);
-              erc721Data.addr.transfer(borrower, erc721Data.nft);
-              return true;
-          } else {
-              revert();
-          }
       }
+      // for lender claim
+      address lender = engine.ownerOf(_index);
+      require(msg.sender == lender, "bad sender");
+      require(isDefaulted(engine, _index), "the loan its not defaulted");
+      require(erc721Data.addr.transferFrom(address(this), lender, erc721Data.nft), "fail transfer");
+      return true;
     }
 }
